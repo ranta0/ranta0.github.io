ce23c3d1eba49a72a267c662550b3835cb46373f
Riccardo Esposito <ric.espo9@gmail.com>
2025-04-29 17:04:48 +0200

    Fix: build type

--- _nuxt/CIHw2bEy.js
+++
@@ -1,1 +0,0 @@
-import{H as t,o as e,j as s,aw as n}from"./CDYtLSyO.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
